0bcf15416c39916a992b2647a8904f5ebfd49d6b
Diego Caceres <dado90caceres@gmail.com>
2014-06-04 19:09:41 -0300

    Se corrige el comportamiento de volar y caer.

--- FelidMecha/Assets/Scripts/PlayerMovement.cs
+++ FelidMecha/Assets/Scripts/PlayerMovement.cs
@@ -3,18 +3,22 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
-	public float flySpeed = 5f;
-	public float forwardSpeed = 1f;
+	public float flySpeed = 15f;
+	public float forwardSpeed = 7f;
+	public float minHeight = -2.7f;
+	public float maxHeight = 5f;
 
 	bool dead = false;
 	bool didJump = false;
 	public bool godMode = false;
+	public int state = 0; //0 idle; 1 run; 2 jump; 3 fall
 
 	Animator animator;
 
 	// Use this for initialization
 	void Start () {
 		animator = transform.GetComponentInChildren<Animator>();
+		
 	}
 	
 	// Do Graphic & Input updates here
@@ -23,36 +27,70 @@ public class PlayerMovement : MonoBehaviour {
 		{
 			didJump = true;
 		}
-		else {
-			//didJump = false;
+		else if((Input.GetMouseButtonUp(0) && !Input.GetKeyDown(KeyCode.Space))
+		        || (!Input.GetMouseButtonDown(0) && Input.GetKeyUp(KeyCode.Space))){
+			didJump = false;
 		}
+
+
+
 	}
 
 	//Do physics engine updates here
 	void FixedUpdate() {
-
+		
 		if(dead)
 			return;
 
-		Vector2 vel = new Vector2();
+		//rigidbody2D.AddForce(Vector2.right * forwardSpeed);
+
+		//rigidbody2D.constantForce = ()
+		Vector2 vel = rigidbody2D.velocity;
 		vel.x = forwardSpeed;
 		rigidbody2D.velocity = vel;
 
 		if(didJump){
-			didJump = false;
-			//vel.y = 0;
-			rigidbody2D.velocity = vel;
 			rigidbody2D.AddForce(Vector2.up * flySpeed);
-
-		}
-
-
-
-		if(animator != null){
-			if(rigidbody2D.velocity.y > 0)
+			if(state != 2){
+				state = 2;
 				animator.SetTrigger("DoFly");
-			else if(rigidbody2D.velocity.y < 0)
-				animator.SetTrigger("DoFall");		
+			}
+		}
+		else if(rigidbody2D.velocity.y <= 0){
+			if(rigidbody2D.velocity.y < 0 && transform.position.y > minHeight){
+				//Esta cayendo
+				if(state != 3){
+					state = 3;
+					animator.SetTrigger("DoFall");
+				}
+			}
+			else if(rigidbody2D.velocity.x > 0){
+				//Esta corriendo
+				if(state != 1){
+					state = 1;
+					animator.SetTrigger("DoRun");
+				}
+			}
+		}	
+
+		//Correct position TOP
+		if(transform.position.y > maxHeight){
+			Vector2 posCorrect = transform.position;
+			posCorrect.y = maxHeight;
+			transform.position = posCorrect;
+			Vector2 velCorrect = rigidbody2D.velocity;
+			velCorrect.y = 0;
+			rigidbody2D.velocity = velCorrect;
+		}
+		
+		//Correct position BOTTOM
+		if(transform.position.y < minHeight){
+			Vector2 posCorrect = transform.position;
+			posCorrect.y = minHeight;
+			transform.position = posCorrect;
+			Vector2 velCorrect = rigidbody2D.velocity;
+			velCorrect.y = 0;
+			rigidbody2D.velocity = velCorrect;
 		}
 
 	}
