1acdf50c1a0bb5990922c19d55aa5538a8622ac3
Diego Caceres <dado90caceres@gmail.com>
2014-06-04 18:00:19 -0300

    Proyecto basico, conteniendo un scroll infinito

---
+++ FelidMecha/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float flySpeed = 5f;
+	public float forwardSpeed = 1f;
+
+	bool dead = false;
+	bool didJump = false;
+	public bool godMode = false;
+
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = transform.GetComponentInChildren<Animator>();
+	}
+	
+	// Do Graphic & Input updates here
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) //Button 0 se supone que es el click izquierdo, y el tap en mobile
+		{
+			didJump = true;
+		}
+		else {
+			//didJump = false;
+		}
+	}
+
+	//Do physics engine updates here
+	void FixedUpdate() {
+
+		if(dead)
+			return;
+
+		Vector2 vel = new Vector2();
+		vel.x = forwardSpeed;
+		rigidbody2D.velocity = vel;
+
+		if(didJump){
+			didJump = false;
+			//vel.y = 0;
+			rigidbody2D.velocity = vel;
+			rigidbody2D.AddForce(Vector2.up * flySpeed);
+
+		}
+
+
+
+		if(animator != null){
+			if(rigidbody2D.velocity.y > 0)
+				animator.SetTrigger("DoFly");
+			else if(rigidbody2D.velocity.y < 0)
+				animator.SetTrigger("DoFall");		
+		}
+
+	}
+
+	void OnCollisionEnter2D(Collision2D collision){
+
+		if(godMode)
+			return;
+
+	}
+}
---
+++ FelidMecha/Assets/Scripts/Tiling.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(SpriteRenderer))]
+
+public class Tiling : MonoBehaviour {
+
+	public int offsetX = 2;				// The offset
+	
+	public int numberOfBackgrounds = 6; 
+
+	public int repeatCount = 0;
+
+	private float spriteWidth = 0f; 
+	private Camera cam;
+
+	void Awake(){
+		cam = Camera.main;
+	}
+
+	// Use this for initialization
+	void Start () {
+		SpriteRenderer sRenderer = GetComponent<SpriteRenderer>();
+		spriteWidth = sRenderer.sprite.bounds.size.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Calculate the cameras extend (half the width) of what the camera can see in world coordinates
+		float camHorizontalExtend = cam.orthographicSize * Screen.width/Screen.height;
+
+		//Calculate the x position where the camera can see the right edge of the sprite (element)
+		float edgeVisiblePositionRight = (transform.position.x + spriteWidth/2) + camHorizontalExtend; 
+
+		//Check if the camera has already passed the edge of the element 
+		if(cam.transform.position.x >= edgeVisiblePositionRight + offsetX)
+		{
+			//We move this sprite to the final at the right
+			Vector3 pos = transform.position;			
+			pos.x += spriteWidth * numberOfBackgrounds;		
+			transform.position = pos;
+			repeatCount++;
+		}
+
+	}
+
+}
